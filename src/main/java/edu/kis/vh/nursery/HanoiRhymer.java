bbd7a1cfc374e96b4260ea96d9b86614fcf04b9d
Bartłomiej Ołubek <bartek.olubek@gmail.com>
2022-01-26 21:59:43 +0100

    1.3.2 #3 Dodanie komentarza na skróty klawiszowe i odpowiedz na pytanie

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -15,3 +15,10 @@ public class HanoiRhymer extends defaultCountingOutRhymer {
 			super.countIn(in);
 	}
 }
+
+// Pytanie: Które wiersze w klasie HanoiRhymer były źle sformatowane? Odp: Były
+// to wiersze 5,12,14 i 15
+
+// Działanie kombinacji klawiszy alt + strzałka w lewo oraz alt + strzałka w
+// prawo polega na przełączaniu się między otwartymi kartami w IDE w prawo lub
+// lewo (zależnie od wybranej kombinacji klawiszy)
