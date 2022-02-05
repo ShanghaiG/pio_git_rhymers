96f4c4b516183243f378517fcb5de7084ff4711c
Bartłomiej Ołubek <bartek.olubek@gmail.com>
2022-02-05 15:43:50 +0100

    #accessory-1.4.3 Dodanie hermetyzacji nieprywatnych atrybutów

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -2,11 +2,36 @@ package edu.kis.vh.nursery.list;
 
 public class Node {
 
-	protected int value;
-	protected Node prev, next;
+	private int value;
+	private Node previousNode;
+	private Node nextNode;
 
 	public Node(int i) {
 		value = i;
+	}
+
+	protected int getValue() {
+		return value;
+	}
+
+	protected void setValue(int value) {
+		this.value = value;
+	}
+
+	protected Node getPreviousNode() {
+		return previousNode;
+	}
+
+	protected void setPreviousNode(Node previousNode) {
+		this.previousNode = previousNode;
+	}
+
+	protected Node getNextNode() {
+		return nextNode;
+	}
+
+	protected void setNextNode(Node nextNode) {
+		this.nextNode = nextNode;
 	}
 
 }
